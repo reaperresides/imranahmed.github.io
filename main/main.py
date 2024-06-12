027ca048c679040b1a6f7051bed90fbd6e653276
imran shaikh <65457907+reaperresides@users.noreply.github.com>
2024-06-12 16:52:26 +0530

    Updated main.py

--- main/main.py
+++ main/main.py
@@ -19,5 +19,5 @@ def portfolio():
     return render_template('portfolio.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,port=port,host="0.0.0.0")
